ac095ea44eb412e8f82e058b7fbccda41e23a544
JuanARojasC <v4zjuanes@gmail.com>
2026-09-19 15:49:43 -0500

    Update API timeout to 30 seconds
    Reduced timeout from 40000 to 30000 milliseconds.

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
   baseURL: 'https://api-ventas-ntxh.onrender.com',
-  timeout: 40000 
+  timeout: 30000 // 30 segundos por si Render se vuelve a dormir
 });
 
 export default api;
